c24bb44df38791cb3adb93b85f88455577d92c7a
antoinel74 <antoine.lansman@gmail.com>
2024-01-29 16:11:50 +0100

    style/header: add clock animation

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DynamicClock } from "./Clock";
+
+export const Header: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-between gap-2 px-4 py-10 md:flex-row md:gap-0 md:px-16">
+      <div className="h-full w-full md:w-1/2">
+        <h1 className="flex flex-col text-[clamp(2.5rem,4vw,5rem)] font-semibold leading-tight">
+          Emporium<span>Winter Co.</span>
+        </h1>
+        <div className="flex items-center gap-2">
+          <span className="block h-2 w-2 animate-spin-slow bg-blue-200"></span>
+          <DynamicClock />
+        </div>
+      </div>
+      <p className="w-full md:w-1/2 xl:text-xl">
+        At <span className="font-bold">Emporium</span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+        unde, magni omnis nisi sapiente quaerat facilis non molestiae. Lorem ipsum dolor sit.
+      </p>
+    </div>
+  );
+};
