5783687847075366b96f662558256538a25590cc
antoinel74 <antoine.lansman@gmail.com>
2024-02-09 15:53:20 +0100

    feat: mobile menu added

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { PrismicNextLink } from "@prismicio/next";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   handleClose: () => void;
@@ -7,21 +9,38 @@ type Props = {
 
 export const MobileMenu: React.FC<Props> = ({ handleClose }) => {
   return (
-    <div className="min-h-screen fixed top-0 left-0 z-[400] w-screen bg-red-200">
+    <div className="min-h-screen fixed top-0 left-0 z-[400] w-screen bg-white">
       <button onClick={handleClose} className="block ml-auto p-4 text-3xl hover:scale-75 transition-all">
         X
       </button>
-      <ul className="flex flex-col">
+      <ul className="mt-12">
         {[
           ["Home", "/"],
           ["Products", "/#products"],
           ["Contact", "/#contact"],
+          ["Join us", "https://instagram.com"],
         ].map(([title, url]) => (
-          <PrismicNextLink key={title} href={url} className="px-2 hover:opacity-75">
+          <PrismicNextLink
+            onClick={handleClose}
+            key={title}
+            href={url}
+            className="block w-full p-4 bg-white hover:invert text-3xl"
+          >
             {title}
           </PrismicNextLink>
         ))}
       </ul>
+      <div className="flex gap-3 absolute bottom-4 right-4">
+        <PrismicNextLink href="https://facebook.com">
+          <Image src="/square-facebook.svg" alt="Facebook" height={30} width={30} />
+        </PrismicNextLink>
+        <PrismicNextLink href="https://facebook.com">
+          <Image src="/square-instagram.svg" alt="Instagram" height={30} width={30} />
+        </PrismicNextLink>
+        <PrismicNextLink href="https://facebook.com">
+          <Image src="/youtube.svg" alt="Youtube" height={40} width={40} />
+        </PrismicNextLink>
+      </div>
     </div>
   );
 };
